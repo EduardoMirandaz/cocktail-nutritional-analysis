1f0cf43832aab81624b6508e28908109118bbd1c
EduardoMirandaz <eduardo.cmazevedo@gmail.com>
2023-07-24 19:50:32 -0300

    Conexao com o banco criada.

---
+++ src/lib/supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+import { SUPABASE_KEY } from '../../supabase_key.js'
+
+const supabaseUrl = 'https://kpqpzztlolviwhdioqlz.supabase.co'
+
+export const supabase = createClient(supabaseUrl, SUPABASE_KEY)
